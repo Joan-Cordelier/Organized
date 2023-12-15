434daebe434572c3a05b8af04f0b34fe96a3cfc0
joan cordelier <joan.cordelier@epitech.eu>
2023-12-15 15:58:04 +0100

    FIX: id implement

--- src/workshop/add.c
+++ src/workshop/add.c
@@ -7,23 +7,17 @@
 
 #include "my_organise.h"
 
-static int get_indice(element_t *data)
+static int check_type(char **str, int ac)
 {
-    int i = 0;
-
-    for (element_t *temp = data; temp != NULL; temp = temp->next)
-        i++;
-    return i;
-}
-
-static int check_type(char *str)
-{
-    if (my_strcmp("Actuator", str) != 0 &&
-        my_strcmp("Device", str) != 0 &&
-        my_strcmp("Processor", str) != 0 &&
-        my_strcmp("Sensor", str) != 0 &&
-        my_strcmp("Wire", str) != 0)
-        return -1;
+    for (int i = 0; i < ac; i += 2) {
+        if ((my_strcmp("Actuator", str[i]) != 0 &&
+            my_strcmp("Device", str[i]) != 0 &&
+            my_strcmp("Processor", str[i]) != 0 &&
+            my_strcmp("Sensor", str[i]) != 0 &&
+            my_strcmp("Wire", str[i]) != 0) ||
+            str[i] == NULL)
+            return -1;
+    }
     return 0;
 }
 
@@ -55,21 +49,22 @@ char *init_str(char *str)
 int add(void *data, char **args)
 {
     int add_nb = tab_len(args);
+    static int id = 0;
     element_t **list = (element_t **) data;
     element_t *new = NULL;
 
-    if (add_nb == -1 || add_nb == 0 || add_nb % 2 != 0)
+    if (add_nb == -1 || add_nb == 0 || add_nb % 2 != 0 ||
+    check_type(args, add_nb) == -1)
         return 84;
     for (int i = 0; i < add_nb; i += 2) {
         new = malloc(sizeof(element_t));
-        new->indice = get_indice(*list);
+        new->indice = id;
         new->name = init_str(args[i + 1]);
-        if (check_type(args[i]) == -1)
-            return 84;
         new->type = init_str(args[i]);
         new->next = *list;
         *list = new;
         disp_add(new);
+        id++;
     }
     return 0;
 }
